6278a05e8381b050042689bd766c62ce16a07cdc
LMTrain <layconmm@msn.com>
2020-06-15 22:07:06 -0500

    Add Spinner Component, Redux thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,54 +1,45 @@
-
-// import { createStore, applyMiddleware, compose } from 'redux'
 import { createStore } from 'redux'
-// import servicesReducer from 'reducers'
 import serviceApp from 'reducers'
 
-// import thunk from 'redux-thunk'
-// import logger from 'redux-logger'
 
-const logger = store => nextDispatch => action  => { 
+const logger = store => nextDispatch => action => {
   console.group(action.type)
-  console.log('%c Prev state', 'color: yellow', store.getState())
-  console.log('%c action', 'color: green', action)
+  console.log('%c prev state', 'color: gray', store.getState())
+  console.log('%c action', 'color: blue', action)
   const returnValue = nextDispatch(action)
-  console.log('%c next state', 'color: cyan', store.getState())
+  console.log('%c next state', 'color: green', store.getState())
   console.groupEnd(action.type)
   return returnValue
 }
-
-const promise = store => nextDispatch => action => {
-  if (typeof action.then === 'function') {
-    return action.then(nextDispatch)
+  
+const thunk = store => nextDispatch => action => {
+  if (typeof action === 'function') {
+    return action(store.dispatch)
+  } else {
+    return nextDispatch(action)
   }
-  return nextDispatch(action)
 }
   
-
-
 const applyMiddlewares = (store, middlewares) => {
   middlewares.slice().reverse().forEach(middleware => {
     store.dispatch = middleware(store)(store.dispatch)
   })
 }
 
+
 const initStore = () => {
-  const middlewares = [promise]
-  
-  const browserSupport = window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()  
+  const middlewares = [thunk]
+ 
+  const browserSupport = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   const store = createStore(serviceApp, browserSupport)
-  if (process.env.NODE_ENV !== 'production') {     
-    middlewares.push(logger)  
+
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(logger)
   }
+
   applyMiddlewares(store, middlewares)
+
   return store
-
 }
+
 export default initStore
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-  // const store = createStore(
-  //   serviceApp, 
-  //   composeEnhancers(applyMiddleware(...middlewares))
-  // )
