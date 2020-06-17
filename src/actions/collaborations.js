cfbb491fd0b988fc6404fd507d3daffc3e3e972a
LMTrain <layconmm@msn.com>
2020-06-16 20:41:14 -0500

    Display messages

--- src/actions/collaborations.js
+++ src/actions/collaborations.js
@@ -1,6 +1,7 @@
 import { 
   COLLABORATION_CREATED_FROM_OFFER, 
-  FETCH_USER_MESSAGES_SUCCESS } from 'types'
+  FETCH_USER_MESSAGES_SUCCESS,
+  MARK_MESSAGE_AS_READ } from 'types'
 import * as api from 'api'
 
 export const collaborate = ({collaboration, message}) => dispatch =>
@@ -21,3 +22,20 @@ export const collaborate = ({collaboration, message}) => dispatch =>
 export const subscribeToMessages = userId => dispatch =>
   api.subscribeToMessages(userId, 
     messages => dispatch({type: FETCH_USER_MESSAGES_SUCCESS, messages}))
+
+
+// export const markMessageAsRead = message => dispatch =>
+//   api.markMessageAsRead(message)
+//      .then(_ => dispatch({type: MARK_MESSAGE_AS_READ, messageId: message.id}))
+
+export const markMessageAsRead = message => api.markMessageAsRead(message)
+
+
+
+
+
+
+
+
+
+
