135e3d2a0b12aaab23d694d3c848b755465d8198
LMTrain <layconmm@msn.com>
2020-06-16 21:30:46 -0500

    Firebase Function and Check user connection

--- src/api/connection.js
+++ src/api/connection.js
@@ -1,7 +1,3 @@
-
-
-
-
 import firebase from 'firebase/app'
 import 'firebase/database'
 
@@ -16,12 +12,6 @@ export const isOnlineForDatabase = {
   state: 'online',
   last_changed: firebase.database.ServerValue.TIMESTAMP
 }
-
-export const isOnlineForFirestore = {
-  state: 'online',
-  last_changed: firebase.firestore.FieldValue.serverTimestamp()
-}
-
 
 export const onConnectionChanged = callback => 
   firebase
